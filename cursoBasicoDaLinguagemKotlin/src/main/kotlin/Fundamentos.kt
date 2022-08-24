11a804759249443702fae16217372a64ebd15f30
Felipe Cesar <felipec.dev@outlook.com>
2022-08-24 11:55:09 -0300

    Mudança de nome

---
+++ cursoBasicoDaLinguagemKotlin/src/main/kotlin/Fundamentos.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+    //Imprime mensagem
+    println("Hello World!")
+    //Imprime mensagem
+    System.out.println("Hello World!")
+
+    val PI = 3.14
+    var raio = 6
+    if(raio < 9){
+        raio += 1
+    }
+    println("O raio é igual a $raio")
+
+}
